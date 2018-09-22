6e79fb99d732258ce219228384a3060d8d49f325
Alyssa Templeton <templetonalyssa@gmail.com>
2018-09-22 14:51:19 -0400

    Hide and Show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,12 @@
-// Pseudo Code
 
-//a timer function for the total quiz
-//a "done" button
 //a questions correct counter, a questions incorrect counter, a questions unanswered counter
 
 // This code will run as soon as the page loads
 window.onload = function() {
-   // $("#lap").on("click", stopwatch.recordLap);
     $("#stop").on("click", stopwatch.stop);
-  //  $("#reset").on("click", stopwatch.reset);
     $("#start").on("click", stopwatch.start);
+    $("#questions").hide();
+    $("#totalscorediv").hide();
   };
   
   //  Variable that will hold our setInterval that runs the stopwatch
@@ -21,50 +18,36 @@ window.onload = function() {
   // Our stopwatch object
   var stopwatch = {
   
-    time: 0,
-   // lap: 1,
-  
-    // reset: function() {
-  
-    //   stopwatch.time = 0;
-    //   stopwatch.lap = 1;
-  
-    //   // DONE: Change the "display" div to "00:00."
-    //   $("#display").text("00:00");
-  
-    //   // DONE: Empty the "laps" div.
-    //   //$("#laps").text("");
-    // },
+    time: 60,
+   
     start: function() {
   
-      // DONE: Use setInterval to start the count here and set the clock to running.
+      // Use setInterval to start the count here and set the clock to running.
       if (!clockRunning) {
         intervalId = setInterval(stopwatch.count, 1000);
         clockRunning = true;
+        $("#questions").show();
       }
     },
     stop: function() {
-  
       // DONE: Use clearInterval to stop the count here and set the clock to not be running.
       clearInterval(intervalId);
       clockRunning = false;
+      $("#questions").hide();
+      $("#totalscorediv").show();
     },
-    // //recordLap: function() {
-  
-    //   // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
-    //   //       and save the result in a variable.
-    //   var converted = stopwatch.timeConverter(stopwatch.time);
-  
-    //   // DONE: Add the current lap and time to the "laps" div.
-    //   $("#laps").append("<p>Lap " + stopwatch.lap + " : " + converted + "</p>");
-  
-    //   // DONE: Increment lap by 1. Remember, we can't use "this" here.
-    //   stopwatch.lap++;
-    // //},
+
     count: function() {
   
       // DONE: increment time by 1, remember we cant use "this" here.
-      stopwatch.time++;
+      stopwatch.time--;
+      console.log(stopwatch.time);
+      if (stopwatch.time===0){
+        stopwatch.stop();
+        $("#questions").hide();
+      $("#totalscorediv").show();
+
+     }
   
       // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
       //       and save the result in a variable.
@@ -93,65 +76,3 @@ window.onload = function() {
       return minutes + ":" + seconds;
     }
   };
-  
-  
-  // Solution if you choose not to put it in an object
-  
-  // var time = 0;
-  // var lap = 1;
-  // function reset() {
-  
-  //   time = 0;
-  //   lap = 1;
-  
-  //   $("#display").text("00:00");
-  //   $("#laps").text("");
-  
-  // }
-  
-  // function start() {
-  //   intervalId = setInterval(count, 1000);
-  // }
-  
-  // function stop() {
-  
-  //   console.log("stopping");
-  //   clearInterval(intervalId);
-  
-  // }
-  
-  // function recordLap() {
-  
-  //   var converted = timeConverter(time);
-  //   $("#laps").append("<p>Lap " + lap + " : " + converted + "</p>");
-  //   lap++;
-  
-  // }
-  
-  // function count() {
-  
-  //   time++;
-  //   var converted = timeConverter(time);
-  //   $("#display").text(converted);
-  
-  // }
-  
-  // function timeConverter(t) {
-  
-  //   var minutes = Math.floor(t / 60);
-  //   var seconds = t - (minutes * 60);
-  
-  //   if (seconds < 10) {
-  //     seconds = "0" + seconds;
-  //   }
-  
-  //   if (minutes === 0) {
-  //     minutes = "00";
-  //   }
-  //   else if (minutes < 10) {
-  //     minutes = "0" + minutes;
-  //   }
-  
-  //   return minutes + ":" + seconds;
-  // }
-  
